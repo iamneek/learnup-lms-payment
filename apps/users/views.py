09f2b3b214d3b917436f64b4fba4528bfd144336
Coolbit <neeoub@gmail.com>
2026-07-07 14:02:56 +0545

    feat: login and authentication works, protected route is accessible with jwt access token

--- apps/users/views.py
+++ apps/users/views.py
@@ -3,17 +3,25 @@ from rest_framework.views import APIView
 
 from rest_framework.response import Response
 from .serializers import RegisterSerializer, UserSerializer
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 
 class RegisterView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
-        serializer = RegisterSerializer(data=request.data)
+        serializer = RegisterSerializer(request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(
                 UserSerializer(serializer.instance).data, status=status.HTTP_201_CREATED
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
