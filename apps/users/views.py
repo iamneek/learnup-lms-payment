f5922ed1b71d6a2e22d7fb6cd5679046667c76df
Coolbit <neeoub@gmail.com>
2026-07-07 12:38:51 +0545

    feat: registration works along with proper serialization

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,10 +1,19 @@
-from rest_framework.views import APIView, Response
-# Create your views here.
+from rest_framework import status
+from rest_framework.views import APIView
+
+from rest_framework.response import Response
+from .serializers import RegisterSerializer, UserSerializer
+from rest_framework.permissions import AllowAny
 
 
-class UserView(APIView):
-    authentication_classes = []
-    permission_classes = []
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
 
-    def get(self, request):
-        return Response({"message": "Hello, World!"})
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                UserSerializer(serializer.instance).data, status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
