34cded2d10678303cc0a783e4f25f60fd36fbc08
Coolbit <neeoub@gmail.com>
2026-07-09 12:48:48 +0545

    fix: lookup course using slug rather than ID

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -8,6 +8,7 @@ from rest_framework.permissions import IsAdminUser, AllowAny
 class CourseViewSet(ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+    lookup_field = "slug"
 
     def get_permissions(self):
         if self.action in ["create", "update", "partial_update", "destroy"]:
