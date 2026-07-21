d393a5aaaea939a530593b4b611b7fd978df53c8
Coolbit <neeoub@gmail.com>
2026-07-21 11:53:20 +0545

    feat: payment views done

--- apps/payments/views.py
+++ apps/payments/views.py
@@ -4,9 +4,12 @@ from .models import Payment
 from .serializers import (
     PaymentAdminSerializer,
     PaymentCreateSerializer,
+    PaymentRejectSerializer,
     PaymentSerializer,
 )
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from .services import approve_payment, reject_payment
+from rest_framework.response import Response
 
 
 class PaymentViewSet(ModelViewSet):
@@ -24,7 +27,7 @@ class PaymentViewSet(ModelViewSet):
     def get_queryset(self):
         user = self.request.user
         base_query = Payment.objects.select_related(
-            "enrollment", "payment_method", "reviwer"
+            "enrollment", "payment_method", "reviewer"
         )
         if getattr(user, "is_staff", False):
             return base_query
@@ -36,9 +39,19 @@ class PaymentViewSet(ModelViewSet):
         return [IsAdminUser()]
 
     @action(detail=True, methods=["post"], url_path="approve")
-    def approve_payment(self, request, pk=None):
-        pass
+    def approve(self, request, pk=None):
+        payment = approve_payment(self.get_object(), request.user)
+        serializer = PaymentAdminSerializer(payment)
+        return Response(serializer.data, status=200)
 
     @action(detail=True, methods=["post"], url_path="reject")
-    def reject_payment(self, request, pk=None):
-        pass
+    def reject(self, request, pk=None):
+        in_serializer = PaymentRejectSerializer(data=request.data)
+        in_serializer.is_valid(raise_exception=True)
+        payment = reject_payment(
+            self.get_object(),
+            request.user,
+            in_serializer.validated_data["rejection_message"],
+        )
+        out_serializer = PaymentAdminSerializer(payment)
+        return Response(out_serializer.data, status=200)
